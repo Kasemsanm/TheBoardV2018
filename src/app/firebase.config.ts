62f40fd3bbef0b6c26d6b6a781551a7db7d39ed8
kasemsanm <32940983+Kasemsanm@users.noreply.github.com>
2018-05-13 14:02:27 +0700

    fix confix firebase angularfire2 v 5

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,8 +1,11 @@
 export const FirebaseConfig = {
-    apiKey: "AIzaSyCYoNkvP7_2osN3o_acfnxKYJT-Z_8Bo0I",
-    authDomain: "theboard2018-46c20.firebaseapp.com",
-    databaseURL: "https://theboard2018-46c20.firebaseio.com",
-    projectId: "theboard2018-46c20",
-    storageBucket: "theboard2018-46c20.appspot.com",
-    messagingSenderId: "1075968767576"
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCYoNkvP7_2osN3o_acfnxKYJT-Z_8Bo0I",
+        authDomain: "theboard2018-46c20.firebaseapp.com",
+        databaseURL: "https://theboard2018-46c20.firebaseio.com",
+        projectId: "theboard2018-46c20",
+        storageBucket: "theboard2018-46c20.appspot.com",
+        messagingSenderId: "1075968767576"
+    }
 }
